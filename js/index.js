10523285a76b463d2edcbd3058697d1580de6830
Anastasia Zaytseva <anastasia.zaytseva0@yandex.ru>
2019-10-18 22:59:46 +0300

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const multimediaScript = document.createElement('script');
-    multimediaScript.setAttribute('src','./js/multimedia.js');
-    const buttonsContainer = document.querySelector('.buttons-wrap');
+import '../css/index.css';
 
+window.addEventListener('DOMContentLoaded', () => {
+    const buttonsContainer = document.querySelector('.buttons-wrap');
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
         '<button class="button">Нет</button>';
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
-});
+
+    const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
+    setTimeout(function() {
+        const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
+        const purchaseListContainer = document.querySelector(".purchase-list-wrap");
+
+        confirmPurchaseButton.onclick = () => {
+            purchaseListContainer.style.display = "block";
+            fridgeInfoContainer.style.display = "none";
+            buttonsContainer.style.display = "none";
+        }
+    }, 500);
+
+    document.querySelector(".header-menu__switcher").addEventListener("click", function () {
+        document.querySelector(".header-menu").classList.toggle("header-menu_active")
+    })
+}, !1);
+
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require('webpack');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+const PATHS = {
+    css: path.join(__dirname)
+};
+
+module.exports = (_, { mode }) => ({
+    devtool: false,
+    entry: './js/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'main.js'
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: [ 'babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: {
+                    loader: 'svg-url-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+        new PurgecssPlugin({
+            paths: glob.sync(`${PATHS.css}/*`, { nodir: true })
+        })
+    ]
+});
